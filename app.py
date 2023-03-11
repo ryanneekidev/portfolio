8be35e871572f6179411887e2b2d01467c4d2d1a
ryanneekidev <127438918+ryanneekidev@users.noreply.github.com>
2023-03-11 06:13:11 +0000

    created about page, linked pages together

--- app.py
+++ app.py
@@ -1,28 +1,14 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    'id':'1',
-    'title': 'Software Engineer',
-    'location': 'remote',
-    'salary': 'MAD11999'
-  },
-  {
-    'id':'2',
-    'title': 'Hardware Engineer',
-    'location': 'remote',
-  }
-]
-
 @app.route("/")
-def listJobsHtml():
-  return render_template('home.html', jobs=JOBS)
+def renderHomepage():
+  return render_template('home.html')
 
-@app.route("/jobs")
-def listJobsApi():
-  return jsonify(JOBS)
+@app.route("/about")
+def renderAboutPage():
+  return render_template('about.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
